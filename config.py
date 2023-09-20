4f059f7819840aaeebc83ebb08ad479adf209db5
Helen Chen <hechen@hmc.edu>
2023-09-20 15:15:16 -0700

    set dates relevant to KCACLARE9

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from datetime import date
 
 # Set Date format like: YYYY, MM, DD
 # Note that FIND_FIRST_DATE uses START_DATE as default start date
-START_DATE = date(2015, 6, 21)
-# START_DATE = date(2010, 1, 1)
-END_DATE = date(2022, 6, 7)
+# START_DATE = date(2015, 6, 21)
+START_DATE = date(2010, 1, 1)
+END_DATE = date(2015, 6, 23)
 
 # set to "metric" or "imperial"
 UNIT_SYSTEM = "metric"
